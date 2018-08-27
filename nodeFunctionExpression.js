1ac3aa95b839b84dea933a61734efaabb6f09fae
geeknitin <geeknitin@users.noreply.github.com>
2018-08-27 22:24:54 +0530

    Adding Node Function Expressions

---
+++ nodeFunctionExpression.js
@@ -0,0 +1,29 @@
+// normal function expression
+
+function sayHi(){
+	console.log(' Hi ');
+}
+
+
+// function invokation
+
+sayHi();
+
+
+// function expression
+var sayBye = function(){
+	console.log("Bye");
+}
+
+sayBye();
+
+
+
+// Passing one function into another function
+function callFunction(fun)
+{
+	fun();
+}
+
+
+callFunction(sayBye);
